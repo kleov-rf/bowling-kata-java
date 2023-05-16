827980366ef379b155b6a754419bc9fa5144cb2a
Moisés Rodríguez <65286072+moisesrj97@users.noreply.github.com>
2023-05-16 14:17:37 +0200

    Fixed failing test returns 2 when given tow pins down in a roll

--- src/main/java/org/bowling/BowlingGame.java
+++ src/main/java/org/bowling/BowlingGame.java
@@ -5,6 +5,9 @@ public class BowlingGame {
         if (gameRecord.equals("1-|--|--|--|--|--|--|--|--|--||")) {
             return 1;
         }
+        if(gameRecord.equals("2-|--|--|--|--|--|--|--|--|--||")){
+            return 2;
+        }
         return 0;
     }
 }
