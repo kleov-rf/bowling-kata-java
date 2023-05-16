b22511c7c725cdc0b6257ac1aead47dcffcd4c68
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2023-05-16 15:51:23 +0200

    Refactored code to improve comprehension and semantics

--- src/main/java/org/bowling/BowlingGame.java
+++ src/main/java/org/bowling/BowlingGame.java
@@ -1,19 +1,29 @@
 package org.bowling;
 
 public class BowlingGame {
+
+    private static boolean isRollNumeric(String roll) {
+        return !(roll.equals("-") || roll.equals("/"));
+    }
+
+    private static boolean isRollSpare(String roll) {
+        return roll.equals("/");
+    }
+
     public static int calculateGameScore(String gameRecord) {
         int score = 0;
 
         String firstFrameFirstRoll = gameRecord.split("")[0];
         String firstFrameSecondRoll = gameRecord.split("")[1];
-        if (!firstFrameFirstRoll.equals("-")) {
+
+        if (isRollNumeric(firstFrameFirstRoll)) {
             score += Integer.parseInt(firstFrameFirstRoll);
         }
-        if (!firstFrameSecondRoll.equals("-") && !firstFrameSecondRoll.equals("/")) {
+        if (isRollNumeric(firstFrameSecondRoll)) {
             score += Integer.parseInt(firstFrameSecondRoll);
         }
 
-        if(firstFrameSecondRoll.equals("/")) {
+        if(isRollSpare(firstFrameSecondRoll)) {
             score = 10;
         }
 
