f9a2776ccbe7033d864ef821593861818aab82f5
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2023-05-16 14:43:25 +0200

    Refactor test cases in order to reduce duplication for single first roll examples

--- src/test/java/org/bowling/BowlingGameShould.java
+++ src/test/java/org/bowling/BowlingGameShould.java
@@ -1,10 +1,23 @@
 package org.bowling;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BowlingGameShould {
+    private static Stream<Arguments> casesForSingleFirstRoll() {
+        String gameRecordFormat = "-|--|--|--|--|--|--|--|--|--||";
+        return Stream.of(
+                Arguments.of("1" + gameRecordFormat, 1),
+                Arguments.of("2" + gameRecordFormat, 2),
+                Arguments.of("3" + gameRecordFormat, 3)
+        );
+    }
 
     @Test
     void should_return_0_when_given_no_strikes_game() {
@@ -16,31 +29,9 @@ public class BowlingGameShould {
         assertEquals(expectedScore, score);
     }
 
-    @Test
-    void should_return_1_when_given_one_pin_down_in_all_frames() {
-        String gameRecord = "1-|--|--|--|--|--|--|--|--|--||";
-        int expectedScore = 1;
-
-        int score = BowlingGame.calculateGameScore(gameRecord);
-
-        assertEquals(expectedScore, score);
-    }
-
-    @Test
-    void should_return_2_when_given_two_pins_down_in_a_roll() {
-        String gameRecord = "2-|--|--|--|--|--|--|--|--|--||";
-        int expectedScore = 2;
-
-        int score = BowlingGame.calculateGameScore(gameRecord);
-
-        assertEquals(expectedScore, score);
-    }
-
-    @Test
-    void should_return_3_when_given_three_pins_down_in_a_roll() {
-        String gameRecord = "3-|--|--|--|--|--|--|--|--|--||";
-        int expectedScore = 3;
-
+    @ParameterizedTest
+    @MethodSource("casesForSingleFirstRoll")
+    void should_return_1_when_given_one_pin_down_in_all_frames(String gameRecord, int expectedScore) {
         int score = BowlingGame.calculateGameScore(gameRecord);
 
         assertEquals(expectedScore, score);
